dc02c50eba0a1da4134badd3172d7b5432de2e9d
AitanMammadova <aitan.mammadova@mail.ru>
2022-11-19 21:57:38 +0400

    Lagrange and Newton

--- lagrange_newton.py
+++ lagrange_newton.py
@@ -31,8 +31,6 @@ def print_fx(f_x):
     print('')
 
 def lagrange_polynomial(x, x_i, f_x):
-    numerator = 1 
-    denumerator = 1
     result = 0
     for i in range(len(x_i)):
         numerator = 1
@@ -48,11 +46,6 @@ def lagrange_polynomial(x, x_i, f_x):
 
 def newton_polynomial(x, x_i, f_x):
     result = f_x[0]
-    xf = 0 
-    xs = 1
-    fxs = 0
-    denumerator = 1
-    # numerator,fxi
     for i in range(1, len(x_i)):
         fxs = 0
         xf = 0
@@ -81,17 +74,6 @@ def opt_xi(n):
         xi.append(math.cos(((2*i+1) * (math.pi)) / (2*(n))))
     return xi
 
-def r(method, f):
-    return method - f
-
-def lagrange(x, x_i, f_x):
-    lagrange_pol = lagrange_polynomial(x, x_i, f_x)
-    return lagrange_pol
-
-def newton(x, x_i, f_x):
-    newton_pol = newton_polynomial(x, x_i, f_x)  
-    return newton_pol
-
 def opt(op_xi, op_fx, x):
     lag_op= lagrange_polynomial(x, op_xi, op_fx)
     new_op = newton_polynomial(x, op_xi, op_fx)
@@ -101,26 +83,31 @@ def print_op_xi_fx(op_xi, op_fx):
     print_xi(op_xi)
     print_fx(op_fx)
     
+def opt_selection(m):
+    print('\nOptimal Secme usulu ucun Y, eks halda N duymesini daxil edin : \nY/N')
+    op = input()
+    if op == 'Y':
+        print_op_xi_fx(op_xi, op_fx)
+        print(f'\nf(x) = {fx(x, function)}')  
+        if m=='l':
+            print(f'Laqranj interpolyasiyasi ile Optimal Secme : {opt(op_xi, op_fx, x)[0]}')
+            print(f'Netice(Laqranj) : {abs(fx(x, function) - opt(op_xi, op_fx, x)[0])}\n')    
+        elif m =='n':
+            print(f'Nyuton interpolyasiyasi ile Optimal Secme : {opt(op_xi, op_fx, x)[1]}')
+            print(f'Netice(Nyuton) : {abs(fx(x, function) - opt(op_xi, op_fx, x)[1])}\n')
+        elif m == 'l_n':
+            print(f'Laqranj interpolyasiyasi ile Optimal Secme : {opt(op_xi, op_fx, x)[0]}')
+            print(f'Netice(Laqranj) : {abs(fx(x, function) - opt(op_xi, op_fx, x)[0])}\n')  
+            print(f'Nyuton interpolyasiyasi ile Optimal Secme : {opt(op_xi, op_fx, x)[1]}')
+            print(f'Netice(Nyuton) : {abs(fx(x, function) - opt(op_xi, op_fx, x)[1])}\n')            
+    elif op == 'N':
+        print('Emeliyyat sonlandirilmisdir!')
+    else: print('Bele bir emeliyyat movcud deyildir!')
+    
 def fx(x, function):
     func = function(x)
     return func
 
-def result_lagrange(x, x_i, f_x, function):
-    print(f'\nLaqranj interpolyasiyasi : {lagrange(x,x_i, f_x)}')
-    print(f'Netice(Laqranj) : {abs(fx(x,function) - lagrange(x, x_i, f_x))}')
-
-def result_newton(x, x_i, f_x, function):
-    print(f'\nNyuton interpolyasiyasi : {newton(x, x_i, f_x)}')
-    print(f'Netice(Nyuton) : {abs(fx(x, function) - newton(x, x_i, f_x))}')
-
-def result_opt_lag(opt, op_xi, op_fx, x, function):
-    print(f'\nLaqranj interpolyasiyasi ile Optimal Secme : {opt(op_xi, op_fx, x)[0]}')
-    print(f'Netice(Laqranj) : {abs(fx(x, function) - opt(op_xi, op_fx, x)[0])}\n')
-    
-def result_opt_new(opt, op_xi, op_fx, x, function):
-    print(f'\nNyuton interpolyasiyasi ile Optimal Secme : {opt(op_xi, op_fx, x)[1]}')
-    print(f'Netice(Nyuton) : {abs(fx(x, function) - opt(op_xi, op_fx, x)[1])}\n')
-    
 
 num_of_x = int(input('X-lerin sayini teyin edin: '))
 x_i = set_x(num_of_x)
@@ -131,52 +118,23 @@ print(f'\nf(x) = {fx(x, function)}')
 op_xi = opt_xi(num_of_x)
 op_fx = get_fx(op_xi, function)
 
-def lagrange_res():
-    result_lagrange(x, x_i, f_x, function)
+print("\nLagranj usulu ucun: 1\nNyuton usulu ucun: 2\nLagranj ve Nyuton usulu ucun: 3")
+method = int(input())
 
-def newton_res():
-    result_newton(x, x_i, f_x, function)
-    
-def optimal_selection(method):
-    print_op_xi_fx(op_xi, op_fx)
-    print(f'\nf(x) = {fx(x, function)}')
-    if(method == 'a'):
-        result_opt_lag(opt, op_xi, op_fx, x, function)
-    elif method == 'b':
-        result_opt_new(opt, op_xi, op_fx, x, function)
-    elif method == 'c':
-        result_opt_lag(opt,op_xi,op_fx, x, function)
-        result_opt_new(opt, op_xi, op_fx, x, function)
-
-print("\nLaqranj interpolyasiyasi ucun: 1 \nNyuton interpolyasiyasi ucun: 2 \nLaqranj ve Nyuton interpolyasiyasi ucun: 3\n")
-method = int(input("Method: "))
-
-match method:
-    case 1 :
-        lagrange_res()
-        print('\nLaqranj interpolyasiyasi ile Optimal Secme usulu ucun Y, eks halda N duymesini daxil edin : \nY/N')
-        op = input()
-        if op == 'Y':
-            optimal_selection('a')       
-        elif op == 'N':
-            print('Emeliyyat sonlandirilmisdir!')
-        else: print('Bele bir emeliyyat movcud deyildir!')
-    case 2 :
-        newton_res()
-        print('\nNyuton interpolyasiyasi ile Optimal Secme usulu ucun Y, eks halda N duymesini daxil edin : \nY/N')
-        op = input()
-        if op == 'Y':
-            optimal_selection('b')
-        elif op == 'N':
-            print('Emeliyyat sonlandirilmisdir!')
-        else: print('Bele bir emeliyyat movcud deyildir!')
-    case 3 :
-        lagrange_res()
-        newton_res()
-        print('\nLaqranj ve Nyuton interpolyasiyasi ile Optimal Secme usulu ucun Y, eks halda N duymesini daxil edin : \nY/N')
-        op = input()
-        if op == 'Y':
-            optimal_selection('c')
-        elif op == 'N':
-            print('Emeliyyat sonlandirilmisdir!')
-        else: print('Bele bir emeliyyat movcud deyildir!')
+match(method):
+    case 1:
+        print(f'\nLaqranj interpolyasiyasi : {lagrange_polynomial(x,x_i, f_x)}')
+        print(f'Netice(Laqranj) : {abs(fx(x,function) - lagrange_polynomial(x, x_i, f_x))}') 
+        opt_selection('l') 
+    case 2:
+        print(f'\nNyuton interpolyasiyasi : {newton_polynomial(x, x_i, f_x)}')
+        print(f'Netice(Nyuton) : {abs(fx(x, function) - newton_polynomial(x, x_i, f_x))}')
+        opt_selection('n')
+    case 3:
+        print(f'\nLaqranj interpolyasiyasi : {lagrange_polynomial(x,x_i, f_x)}')
+        print(f'Netice(Laqranj) : {abs(fx(x,function) - lagrange_polynomial(x, x_i, f_x))}')
+        print(f'\nNyuton interpolyasiyasi : {newton_polynomial(x, x_i, f_x)}')
+        print(f'Netice(Nyuton) : {abs(fx(x, function) - newton_polynomial(x, x_i, f_x))}') 
+        opt_selection('l_n')   
+    case other :
+        print('Bele bir emeliyyat movcud deyildir!')           
